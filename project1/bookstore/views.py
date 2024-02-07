caf9d929c5356dbe6a638b56918a77f9765e4350
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-07 16:23:54 +0530

    added the average and ordering

--- project1/bookstore/views.py
+++ project1/bookstore/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from .models import Book
+from django.db.models import Avg
 # Create your views here.
 def index(request):
-    books=Book.objects.all()
-    return render(request,"index.html",{"books":books})
+    books=Book.objects.all().order_by("rating")
+    return render(request,"index.html",{"books":books,
+                                        "averagerating":books.aggregate(Avg("rating"))
+                                        })
 def postdetails(request,id):
     book=Book.objects.get(pk=id)
     return render(request,"book-detail.html",{
+        "id":id,
         "title":book.title,
         "author":book.author,
         "rating":book.rating,
-        "isbestseller":book.isbestselling
+        "isbestseller":book.isbestselling,
+        "slugify":book.slug
     })
