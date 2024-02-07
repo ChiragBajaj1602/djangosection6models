f1fcabd9c478d6fdd5dc23349f424399b00f59a7
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-07 12:50:17 +0530

    acessed db by the template

--- project1/bookstore/views.py
+++ project1/bookstore/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+def index(request1):
+    books=Book.objects.all()
+    return render(request1,"index.html",{"books":books})
