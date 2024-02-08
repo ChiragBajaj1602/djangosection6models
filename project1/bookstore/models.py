dfd63058f023cf24dbfe955c4f2aa7bd2015f5be
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-08 16:04:23 +0530

    began section 8 for the relatioships

--- project1/bookstore/models.py
+++ project1/bookstore/models.py
@@ -3,10 +3,15 @@ from django.core.validators import MinValueValidator,MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
+class Author(models.Model):
+    fname=models.CharField(max_length=50,null=True)
+    lname=models.CharField(max_length=51,null=True)
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
 class Book(models.Model):
     title=models.CharField(max_length=50)
     rating=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author=models.CharField(null=True,max_length=100)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
     isbestselling=models.BooleanField(default=False)
     slug=models.SlugField(default="",null=False,blank=True)
 
@@ -17,3 +22,6 @@ class Book(models.Model):
     def save(self,*args,**kwargs):
         self.slug=slugify(self.title)
         super().save(*args,**kwargs)
+class Fruit(models.Model):
+    name=models.CharField(max_length=100)
+
