54caa1756f754a87480a497f47191b0971226314
Chirag Bajaj <chirag.bajaj@hcode.tech>
2024-02-09 11:12:07 +0530

    added the one to one relationship between author model and address model and added the meta class

--- project1/bookstore/admin.py
+++ project1/bookstore/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import Book,Author
+from .models import Book,Author,Address
 # Register your models here.
 class Bookadmin(admin.ModelAdmin):
     prepopulated_fields={"slug":("title",)}
     list_display=("title","author")
     list_filter=("author","rating")
-
+class AddressAdmin(admin.ModelAdmin):
+    pass
 class Author_Admin(admin.ModelAdmin):
     list_display=("lname","fname")
 admin.site.register(Book,Bookadmin)
 admin.site.register(Author,Author_Admin)
+admin.site.register(Address,AddressAdmin)
--- project1/bookstore/models.py
+++ project1/bookstore/models.py
@@ -9,12 +9,15 @@ class Address(models.Model):
     city=models.CharField(max_length=80)
     def __str__(self):
         return f'{self.street} {self.city} {self.postal_code}'
+    class Meta:
+        verbose_name="Rasta"
+        verbose_name_plural="Address Entries"
 class Author(models.Model):
     fname=models.CharField(max_length=50,null=True)
     lname=models.CharField(max_length=51,null=True)
-    address=models.OneToOneField(Address,on_delete=models.CASCADE,null=True)
+    address=models.OneToOneField(Address,on_delete=models.CASCADE,null=True,related_name="author")
     def __str__(self):
-        return f'{self.fname} {self.lname}'
+        return f'{self.fname} {self.lname} {self.address}'
 class Book(models.Model):
     title=models.CharField(max_length=50)
     rating=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
